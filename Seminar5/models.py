6eeb9cdda3f12c57d0bbb042cf29980441ac09e8
Gereltuya <qerelt@gmail.com>
2019-07-26 10:47:52 +0800

    add models.py for hint

---
+++ Seminar5/models.py
@@ -0,0 +1,198 @@
+# Size notations:
+# B = batch_size, H = hidden_size, M = block_size, L = attn_span
+
+
+def _skew(X, pad_value):
+    """shift every row 1 step to right"""
+    # X = B x M x L
+    B, M, L = X.size()
+    X = F.pad(X, (0, M + 1), value=pad_value)  # B x M x (L+M+1)
+    X = X.view(B, -1)  # B x ML+MM+M
+    X = X[:, :-M]  # B x ML+MM
+    X = X.view(B, M, M + L)  # B x M x L+M
+    return X
+
+
+def _unskew(X):
+    """reverse _skew operation"""
+    # X = B x M x L+M
+    B, M, L = X.size()
+    L -= M
+    X = X.view(B, -1)  # B x ML+MM
+    X = F.pad(X, (0, M))  # B x ML+MM+M
+    X = X.view(B, M, M + L + 1)  # B x M x L+M+1
+    X = X[:, :, :L]  # B x M x L
+    return X
+
+
+class SeqAttention(nn.Module):
+    """Sequential self-attention layer.
+    Each token will attend to its previous fixed number of steps.
+    Note that attention doesn't include the current step itself.
+    """
+    def __init__(self, hidden_size, attn_span,
+                 dropout, adapt_span_params, **kargs):
+        nn.Module.__init__(self)
+        self.dropout = nn.Dropout(dropout)
+        self.hidden_size = hidden_size # size of a single head
+        self.attn_span = attn_span
+
+    def forward(self, query, key, value, key_pe):
+        # query size = B x M x H
+        # key, value sizes = B x (M+L) x H
+
+        # compute attention from context
+        # B x M (dest) x (M+L) (src)
+        attn_cont = torch.matmul(query, key.transpose(-1, -2))
+        attn_cont = _unskew(attn_cont)  # B x M x L
+
+        # compute the effect of position embedding
+        attn_pos = torch.matmul(query, key_pe)  # B x M x L_pos
+        attn = attn_cont + attn_pos
+
+        # TODO 1
+        # Scale attention scores with square root of d_k. HINT: Result dimention # B x M X L_pos
+        attn = ?
+
+        # TODO 2
+        # Compute softmax on attentions scores
+        attn = ?
+
+        # TODO 3
+        # For regularization purpose, use dropout on attention scores. HINT: result dimention B x M X L_pos
+        attn = ?
+        attn_cont = _skew(attn, 0)  # B x M X (L+M)
+
+        # TODO 4
+        # Compute attention output using attention scores and value vectors. HINT: result dimention B x M x H
+        out  = ?
+
+        return out
+
+    def get_cache_size(self):
+        return self.attn_span
+
+
+class MultiHeadSeqAttention(nn.Module):
+    def __init__(self, hidden_size, nb_heads, **kargs):
+        nn.Module.__init__(self)
+        assert hidden_size % nb_heads == 0
+        self.nb_heads = nb_heads
+        self.head_dim = hidden_size // nb_heads
+        self.attn = SeqAttention(
+            hidden_size=self.head_dim, nb_heads=nb_heads, **kargs)
+        self.proj_query = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.proj_out = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.proj_val = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.proj_key = nn.Linear(hidden_size, hidden_size, bias=False)
+
+    def head_reshape(self, x):
+        K = self.nb_heads
+        D = self.head_dim
+        x = x.view(x.size()[:-1] + (K, D))  # B x (M+L) x K x D
+        x = x.transpose(1, 2).contiguous()  # B x K x (M+L) x D
+        x = x.view(-1, x.size(-2), x.size(-1))  # B_K x (M+L) x D
+        return x
+
+    def forward(self, query, key, value, key_pe):
+        B = query.size(0)
+        K = self.nb_heads
+        D = self.head_dim
+        M = query.size(1)
+
+        query = self.proj_query(query)
+        query = self.head_reshape(query)
+        value = self.proj_val(value)
+        value = self.head_reshape(value)
+        key = self.proj_key(key)
+        key = self.head_reshape(key)
+
+        out = self.attn(query, key, value, key_pe)  # B_K x M x D
+        out = out.view(B, K, M, D)  # B x K x M x D
+        out = out.transpose(1, 2).contiguous()  # B x M x K x D
+        out = out.view(B, M, -1)  # B x M x K_D
+        out = self.proj_out(out)
+        return out
+
+
+class FeedForwardLayer(nn.Module):
+    def __init__(self, hidden_size, inner_hidden_size, dropout, **kargs):
+        nn.Module.__init__(self)
+        self.fc1 = nn.Linear(hidden_size, inner_hidden_size)
+        self.fc2 = nn.Linear(inner_hidden_size, hidden_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, h):
+        # TODO 5
+        # Using self.fc1 to linearly tranform attention output, h.
+        # Use activation function RELU
+        h1 =
+
+        # TODO 6
+        # For regularization purpose, add dropout to h1 above.
+        h1 =
+
+        # TODO 7
+        # Using self.fc2 to linearly tranform h1.
+        h2 =
+
+        return h2
+
+
+class TransformerSeqLayer(nn.Module):
+    def __init__(self, hidden_size, **kargs):
+        nn.Module.__init__(self)
+        self.attn = MultiHeadSeqAttention(hidden_size=hidden_size, **kargs)
+        #self.attn = SeqAttention(hidden_size=hidden_size, **kargs)
+        self.ff = FeedForwardLayer(hidden_size=hidden_size, **kargs)
+        self.norm1 = nn.LayerNorm(hidden_size)
+        self.norm2 = nn.LayerNorm(hidden_size)
+
+    def forward(self, h, h_cache, key_pe):
+        # h = B x M x H
+        # h_cache = B x L x H
+        h_all = torch.cat([h_cache, h], dim=1)  # B x (M+L) x H
+        attn_out = self.attn(h, h_all, h_all, key_pe)
+        h = self.norm1(h + attn_out)  # B x M x H
+        ff_out = self.ff(h)
+        out = self.norm2(h + ff_out)  # B x M x H
+        return out
+
+
+class TransformerSeq(nn.Module):
+    def __init__(self, vocab_size, hidden_size, nb_heads, nb_layers,
+                 attn_span, **kargs):
+        nn.Module.__init__(self)
+        # token embeddings
+        self.in_emb = nn.Embedding(vocab_size, hidden_size)
+        self.out_emb = nn.Linear(hidden_size, vocab_size)
+        # position embeddings
+        self.key_pe = nn.Parameter(
+            torch.randn(1, hidden_size // nb_heads, attn_span))
+
+        self.layers = nn.ModuleList()
+        self.layers.extend(
+            TransformerSeqLayer(
+                hidden_size=hidden_size, nb_heads=nb_heads,
+                attn_span=attn_span, **kargs)
+            for _ in range(nb_layers))
+
+    def forward(self, x, h_cache):
+        # x size = B x M
+        block_size = x.size(1)
+        h = self.in_emb(x)  # B x M x H
+        h_cache_next = []
+        for l, layer in enumerate(self.layers):
+            cache_size = layer.attn.attn.get_cache_size()
+            if cache_size > block_size:
+                h_cache_next_l = torch.cat(
+                    [h_cache[l][:, -cache_size + block_size:, :], h],
+                    dim=1).detach()
+            else:
+                h_cache_next_l = h[:, -cache_size:, :].detach()
+            h_cache_next.append(h_cache_next_l)
+            h = layer(h, h_cache[l], self.key_pe)  # B x M x H
+
+        out = F.log_softmax(self.out_emb(h), dim=-1)
+
+        return out, h_cache_next
